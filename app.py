f5ac66d95d6299ebcdd344661888c194ce7e2f53
PedroVA95 <203375557+PedroVA95@users.noreply.github.com>
2025-06-09 20:44:29 -0600

    movement_app

---
+++ app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.header('Contruir un histograma de año de modelo del vehiculo')
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+hist_button = st.button('Construir histograma')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="model_year", color="condition")
+
+    # mostrar un gráfico Plotly interactivo d
+    st.plotly_chart(fig, use_container_width=True)
